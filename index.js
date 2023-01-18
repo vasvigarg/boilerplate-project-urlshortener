95fec71a1ab10c8097f4f2541d6e6765af6348da
VasviGarg <gargvasvi2004@gmail.com>
2023-01-18 09:50:27 +0000

    index.js edited

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
-
+app.use(express.json());
+app.use(express.urlencoded());
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +19,40 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const originalUrls = []
+const shortUrls = []
+
+app.post("/api/shorturl", function(req,res){
+  const url = req.body.url
+  const foundIndex = originalUrls.indexOf(url)
+  if(!url.includes("https://") && !url.includes("http://")) {
+    return res.json({error: "invalid url" })
+  }
+  if(foundIndex<0) {
+    originalUrls.push(url)
+    shortUrls.push(shortUrls.length)
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length - 1
+    })
+  }
+  return res.json({
+    original_url: url,
+    short_url: shortUrls[foundIndex]
+  })
+})
+
+app.get("/api/shorturl/:shorturl", function(req,res) {
+  const shorturl = parseInt(req.params.shorturl)
+  const foundIndex = shortUrls.indexOf(shorturl)
+  if(foundIndex<0) {
+    return res.json({
+      "error": "No short URL found for the given input"
+    })
+  }
+  res.redirect(originalUrls[foundIndex])
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
